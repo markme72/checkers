f236c3d3433a371e057662fd190d7a7edf892da2
Mike <Mike@Mike>
2013-10-22 11:42:06 -0600

    GamePreferencesMenuControl and GamePreferencesMenuView Mike Coleman

---
+++ src/checkers/GamePreferencesMenuControl.java
@@ -0,0 +1,193 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Checkers;
+
+import java.awt.Dimension;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Mike Coleman
+ */
+public class GamePreferencesMenuControl {
+    
+    private Game game;
+
+    public GamePreferencesMenuControl() {
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+    
+       
+    
+    public void getMarker(Player player) {
+        String marker = null;
+                
+        boolean valid = false;
+        do {
+            System.out.println("\n\t" + player.getName() 
+                + ", enter a single character that will be used to mark your squares in the game.");
+            Scanner in = Checkers.getInputFile();
+            marker = in.nextLine();
+            if (marker == null  || marker.length() < 1) {
+                continue;
+            }
+            
+            marker = marker.trim().substring(0, 1);
+            marker = marker.toUpperCase();
+            valid = this.saveMarker(player, marker);
+        } while (!valid);
+
+        this.saveMarker(player, marker);
+   
+    }
+    
+     public boolean getDimensions() {
+
+        if (game.getStatus().equals(Game.PLAYING)) {
+            new CheckersError().displayError("You can not change the dimensions "
+              + "of the board once the game has been started. "
+              + "\n\tStart a new game and then change the dimensions "
+              + "of the board. ");
+            return false;
+        }
+
+        // prompt for the row and column numbers
+        System.out.println("\n\tEnter the number of rows and columns in the board (For example: 3 3)");
+
+        Scanner inFile = Checkers.getInputFile(); // get input file 
+        
+        // read the row and column coordinates
+        String[] valuesEntered;
+        Dimension dimension = null;
+        do {
+            String strNoRowsColumns = inFile.nextLine(); // read in row and column
+            strNoRowsColumns = strNoRowsColumns.trim(); // trim all blanks from front and end 
+            strNoRowsColumns = strNoRowsColumns.replace(',', ' '); // replace commas with a blank
+            valuesEntered = strNoRowsColumns.split("\\s"); // tokenize the string
+
+            if (valuesEntered.length < 1) {
+                new CheckersError().displayError(
+                        "You must enter two numbers, the number rows and columns, "
+                        + "or a \"Q\" to quit. Try again.");
+                continue;
+            } else if (valuesEntered.length == 1) {
+                if (valuesEntered[0].toUpperCase().equals("Q")) { // Quit?
+                    return false;
+                }  // wrong number of values entered.
+     
+                // wrong number of values entered.
+                new CheckersError().displayError(
+                       "You must enter two numbers, the number rows and columns, "
+                       + "or a \"Q\" to quit. Try again.");
+                continue;
+            }
+            
+            // user java regular expression to check for valid integer number?
+            Pattern digitPattern = Pattern.compile(".*\\D.*");
+            if (digitPattern.matcher(valuesEntered[0]).matches()  || 
+                digitPattern.matcher(valuesEntered[1]).matches()
+               ) {
+                new CheckersError().displayError(
+                        "You must enter two numbers, the number rows and columns, "
+                        + "or a \"Q\" to quit. Try again.");
+                continue;
+            }
+            
+            int rowsEntered = Integer.parseInt(valuesEntered[0]);
+            int columnsEntered = Integer.parseInt(valuesEntered[1]);
+            dimension = new Dimension(rowsEntered, columnsEntered);
+
+        } while (dimension == null);
+        
+        this.saveDimensions(dimension);
+        
+        return true;
+    }
+   
+    
+    
+    
+    private boolean saveMarker(Player player, String marker) { 
+
+        if (player == null  ||  marker == null) {
+            new CheckersError().displayError("saveMarker - player or marker is invalid");
+            return false;
+        }
+
+   
+        if (game.getPlayerA().getMarker().equals(game.getPlayerB().getMarker())) {
+            new CheckersError().displayError("Both players can not use the same character for a marker.");
+            return false;
+        }
+        
+        // update the players markers
+        player.setMarker(marker);
+        
+        return true;
+    }
+        
+    
+    private boolean saveDimensions(Dimension dimension)  {
+        // validate inputs
+        if (this.game.getStatus().equals(Game.PLAYING)) {
+            new CheckersError().displayError("You can not change the dimensions "
+              + "of the board once the game has been started. "
+              + "\nStart a new game and then change the dimensions "
+              + "of the board. ");
+            return false;
+        }
+        
+        
+        if (dimension == null) {
+            new CheckersError().displayError(
+                    "The number of rows must be between 3 -10 and the "
+                    + "number of columns must be between 3 -10 ");
+            return false;
+        }
+        
+        int boardRowCount = dimension.width;
+        int boardColumnCount= dimension.height;
+        
+        if (boardRowCount < 3 || boardRowCount > 10) {
+            new CheckersError().displayError(
+                    "The number of rows must be between 3 -10 and the "
+                    + "number of columns must be between 3 -10 ");
+            return false;
+        }
+
+        if (boardColumnCount < 3 || boardColumnCount > 10) {
+            new CheckersError().displayError(
+                    "The number of rows must be between 3 -10 and the "
+                    + "number of columns must be between 3 -10 ");
+            return false;
+        }
+
+        // no change in the board size so return
+        if (boardRowCount == this.game.getBoard().getRowCount() &&
+            boardColumnCount == this.game.getBoard().getColumnCount()) {
+            return true;
+        }
+        
+        // change the size board
+        Player[][] boardLocations = new Player[boardRowCount][boardColumnCount];
+        this.game.getBoard().getBoardDimensions().setLocation(boardRowCount, boardRowCount);
+        this.game.getBoard().setBoardLocations(boardLocations);
+        
+        Dimension boardDimensions = new Dimension(boardRowCount, boardColumnCount);
+        
+        return true;
+    }
+    
+    
+    
+}
--- src/checkers/GamePreferencesMenuView.java
+++ src/checkers/GamePreferencesMenuView.java
@@ -2,17 +2,104 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+package Checkers;
 
-package checkers;
+
+import java.util.Scanner;
 
 /**
  *
- * @author Mark Earl
+ * @author Mike Coleman
  */
-public class GamePreferencesMenuView {
+
+
+
+
+public class GamePreferencesMenuView  {
+    
+    private Game game;
+    private GamePreferencesMenuControl gamePreferenceControl = new GamePreferencesMenuControl();
+
+    private final static String[][] menuItems = {
+        {"1", "Change Marker of the first Player"},
+        {"2", "Change Marker of the second Player"},
+        {"D", "Change the dimensions of the board"},
+        {"Q", "Return to game menu"}
+    };
+
+    public GamePreferencesMenuView() {
+    }
 
     
-    public Game getInput (Game game) {
-        return game;
+    public String getInput(Object object) {       
+        this.game = (Game) object;
+        this.gamePreferenceControl.setGame(game);
+        
+        String gameStatus = Game.PLAYING;
+        do {
+            this.display();
+
+            // get commaned entered
+            String command = this.getCommand();
+            
+            switch (command) {
+                case "1":
+                    this.gamePreferenceControl.getMarker(this.game.getPlayerA());
+                    break;
+                case "2":
+                    this.gamePreferenceControl.getMarker(this.game.getPlayerB());
+                    break;
+                case "D":
+                    this.gamePreferenceControl.getDimensions();
+                    break;
+                case "Q":
+                    return Game.QUIT;
+            }
+        } while (!gameStatus.equals(Game.QUIT));
+
+        return gameStatus;
     }
+    
+    
+        
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < GamePreferencesMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+
+    private boolean validCommand(String command) {
+        String[][] items = GamePreferencesMenuView.menuItems;
+
+        for (String[] item : GamePreferencesMenuView.menuItems) {
+            if (item[0].equals(command)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected final String getCommand() {
+
+        Scanner inFile = Checkers.getInputFile();
+        String command;
+        boolean valid = false;
+        do {
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            valid = validCommand(command);
+            if (!validCommand(command)) {
+                new CheckersError().displayError("Invalid command. Please enter a valid command.");
+                continue;
+            }
+                
+        } while (!valid);
+        
+        return command;
+    }
+ 
 }
